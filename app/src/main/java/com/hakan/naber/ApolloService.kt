2b8eb4fcc7752c8593a9852c7a0c78e3c9fdd851
hakan <hakan.suluoglu@donetr.com>
2020-03-24 21:10:50 +0300

    initialize coroutines

--- app/src/main/java/com/hakan/naber/ApolloService.kt
+++ app/src/main/java/com/hakan/naber/ApolloService.kt
@@ -1,8 +1,12 @@
 package com.hakan.naber
 
+import androidx.lifecycle.MutableLiveData
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.coroutines.toDeferred
-import com.hakan.GetMessageQuery
+import com.apollographql.apollo.exception.ApolloHttpException
+import com.hakan.CreateMessageMutation
+import com.hakan.GetAllMessagesQuery
+import com.hakan.fragment.MessageFragment
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
@@ -13,19 +17,55 @@ class ApolloService(
 ) :  NaberDataSource(apolloClient){
     private var job: Job? = null
 
-    override fun getMessages() {
-        val repositoriesQuery = GetMessageQuery.builder().id("hey").build()
+    override fun getMessages() : MutableLiveData<Resource<List<MessageFragment>>> {
+        val result = MutableLiveData<Resource<List<MessageFragment>>>()
+        val getAllMessagesQuery = GetAllMessagesQuery.builder().build()
 
-        job = CoroutineScope(processContext).launch {
+        GlobalScope.launch {
             try {
-                val response = apolloClient.query(repositoriesQuery).toDeferred().await()
+                val response = apolloClient.query(getAllMessagesQuery).toDeferred().await()
                 withContext(resultContext) {
-                    println(response)
+                    println()
+                    result.value = Resource.success(mapRepositoriesResponseToRepositories(response))
                 }
             } catch (e: Exception) {
-                println(e)
+                withContext(resultContext) {
+                    if (e is ApolloHttpException)
+                        result.value = Resource.error(
+                            "${e.message} | code ${e.rawResponse()?.code}",
+                            0
+                        )
+                    else
+                        result.value = Resource.error("${e.message}", 0)
+                }
             }
         }
+        return result;
+    }
 
+    override fun createMessage(body: String): MutableLiveData<Resource<MessageFragment>> {
+        val result = MutableLiveData<Resource<MessageFragment>>()
+        val createMessageMutation = CreateMessageMutation.builder().body(body).build()
+
+        GlobalScope.launch {
+            try {
+                val response = apolloClient.mutate(createMessageMutation).toDeferred().await()
+                withContext(resultContext) {
+                    println()
+                    result.value = Resource.success(mapCreateMessageToMessage(response))
+                }
+            } catch (e: Exception) {
+                withContext(resultContext) {
+                    if (e is ApolloHttpException)
+                        result.value = Resource.error(
+                            "${e.message} | code ${e.rawResponse()?.code}",
+                            0
+                        )
+                    else
+                        result.value = Resource.error("${e.message}", 0)
+                }
+            }
+        }
+        return result;
     }
 }
--- app/src/main/java/com/hakan/naber/MainActivity.kt
+++ app/src/main/java/com/hakan/naber/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.hakan.naber
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        dataSource.getMessages()
-    }
+        dataSource.getMessages().observe(this, Observer {
+            when (it?.status) {
+                Resource.LOADING -> {
+                    Log.d("MainActivity", "--> Loading...")
+                }
+                Resource.SUCCESS -> {
+                    Log.d("MainActivity", "--> Success! | loaded ${it.data?.size ?: 0} records.")
+                }
+                Resource.ERROR -> {
+                    Log.d("MainActivity", "--> Error!")
+                    Toast.makeText(this, "Error: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+     }
+
+
+
 }
